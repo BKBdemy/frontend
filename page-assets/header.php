47b4cae48e26803a5f9021e3b0310f885899b5bc
TimothyRoth <roth@marketport.de>
2023-02-16 11:33:03 +0100

    fixed absolute and relative pathing issues

--- page-assets/header.php
+++ page-assets/header.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BKBdemy</title>
-    <script src="script.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <script src="dist/main.min.js"></script>
+    <link rel="stylesheet" href="dist/main.min.css">
 </head>
 <body>
 <header></header>
